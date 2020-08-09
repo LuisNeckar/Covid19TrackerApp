a7783ff441f75f85e1bfc3e96d7307fa816efa14
Luis Neckar <luis.nekcar@gmail.com>
2020-08-09 12:34:21 +0200

    undo redundance

--- src/main/java/io/luisneckar/covid19tracker/controllers/HomeController.java
+++ src/main/java/io/luisneckar/covid19tracker/controllers/HomeController.java
@@ -32,18 +32,5 @@ public class HomeController {
 
         return "home";
     }
-
-    @GetMapping("secondPage")
-    public String home(Model model) {
-        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
-        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
-        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
-
-        model.addAttribute("totalNewCases", totalNewCases);
-        model.addAttribute("totalReportedCases", totalReportedCases);
-        model.addAttribute("locationStats", allStats);
-
-        return "home";
-    }
 }
 
